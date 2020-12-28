c64bd9c38f125497818e4ddffd4dc69ccd75b577
Martin Carlsson <martin@funnel.io>
2020-12-28 11:14:13 +0100

    capitalize.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,30 @@ fn main() {
     println!("Hello, what's your name?");
     let your_name = what_is_your_name();
 
-    println!("Hello, {}!", your_name);
+    println!("Hello, {}!", capitalize_first(&your_name));
+
+    if your_name == "bert" {
+        println!("Welcome!");
+    } else {
+        println!("Sorry, you are not welcome!")
+    }
 }
 
+fn capitalize_first(s: &str) -> String {
+    let mut chars = s.chars();
+    chars
+        .next()
+        .map(|first_letter| first_letter.to_uppercase())
+        .into_iter()
+        .flatten()
+        .chain(chars)
+        .collect()
+}
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
 
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    your_name
+    your_name.trim().to_lowercase()
 }
