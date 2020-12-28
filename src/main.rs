fa27f6f56a3a17deb9fad5ec6816dc460289a834
Martin Carlsson <martin@funnel.io>
2020-12-28 10:38:27 +0100

    function.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use std::io::stdin;
 
 fn main() {
     println!("Hello, what's your name?");
+    let your_name = what_is_your_name();
+
+    println!("Hello, {}!", your_name);
+}
+
+fn what_is_your_name() -> String {
     let mut your_name = String::new();
 
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-
-    println!("Hello, {}!", your_name);
+    your_name
 }
