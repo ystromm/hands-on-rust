b3111feae60b31c2b181bae3fe028d8f2d00bdeb
Martin Carlsson <martin@funnel.io>
2020-12-29 15:42:52 +0100

    Started on dungeon crawler.

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl State {
+    fn new() -> Self { State {} }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {}
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .build()?;
+    main_loop(context, State::new())
 }
